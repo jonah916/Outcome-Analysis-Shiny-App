be97aac7a37afb02eee738ae91299c5681ada78e
Jonah Wiener-Brodkey <58953110+jonah916@users.noreply.github.com>
2022-02-16 22:38:08 -0800

    Update 916InkShinyApp2_1.R
    Added more comments for better readability.

--- 916InkShinyApp2_1.R
+++ 916InkShinyApp2_1.R
@@ -1,3 +1,5 @@
+# Load packages
+
 library(shiny)
 library(openxlsx)
 library(knitr)
@@ -12,29 +14,34 @@ library(cowplot)
 # Import and Data Prep
 
 final_app_df_10.11.19 <- read.xlsx("FinalDF2.xlsx")
-final_app_df_10.11.19$SurveyDate <- convertToDate(final_app_df_10.11.19$SurveyDate)
-response_codes <- read.xlsx("916InkSurveyResponseCodes.xlsx", sheet = 3)
+final_app_df_10.11.19$SurveyDate <- convertToDate(final_app_df_10.11.19$SurveyDate) # convert dates to Date data type
+response_codes <- read.xlsx("916InkSurveyResponseCodes.xlsx", sheet = 3) # data frame containing the desired direction of change for each survey question
 
 
 # Define main function
 
 get_pct_change3 <- function(df){
+  # mean values for pre-surveys
   pre <- as.data.frame(df) %>%
     filter(., PrePost == 1) %>%
-    select(., 2:38) %>%
+    select(., 2:38) %>% # refine to relevant columns
     apply(., 2, mean, na.rm = TRUE)
+  # mean values for post surveys
   post <- as.data.frame(df) %>%
     filter(., PrePost == 2) %>%
-    select(., 2:38) %>%
+    select(., 2:38) %>% # refine to relevant columns
     apply(., 2, mean, na.rm = TRUE)
+  # create table showing pre and post means for each survey question
   report <- cbind(Questions = names(pre),
                   Avg.Pre.Value = as.vector(pre),
                   Avg.Post.Value = as.vector(post)) %>%
     as.data.frame(., stringsAsFactors = FALSE)
   class(report$Avg.Pre.Value) <- "numeric"
   class(report$Avg.Post.Value) <- "numeric"
+  # add column for percent change between pre- and post-surveys
   report <- report %>%
     mutate(Pct.Change = percent((Avg.Post.Value-Avg.Pre.Value)/Avg.Pre.Value))
+  # join report to response codes DF to show whether change is good or bad
   report <- left_join(report, response_codes[,c("Questions", "ImproveDirection")], by = "Questions")
   report
 }
@@ -43,57 +50,72 @@ get_pct_change3 <- function(df){
 # UI
 
 ui <- fluidPage(
-  titlePanel("Tool for Demographic Analysis of 916 Ink Participants"),
+  titlePanel("Tool for Demographic Analysis of 916 Ink Participants"), # App title
   sidebarLayout(
     sidebarPanel(
+      # Update Button
       actionButton(inputId = "update",
                    label = "Update"),
       p("Customize the checkboxes and click \"Update\" to apply your changes. The table may take a moment to render."),
+      # Grade in school checkboxes
       checkboxGroupInput(inputId = "grade",
                          label = "Grade Level: ",
                          choices = sort(unique(final_app_df_10.11.19$Grade)),
                          inline = TRUE),
+      # Select All button for grades
       actionButton(inputId = "gradeSA",
                    label = "Select All"),
+      # Gender checkboxes
       checkboxGroupInput(inputId = "gender",
                          label = "Gender: ",
                          choices = list("M", "F", "Decline"),
                          inline = TRUE),
+      # Select All button for gender
       actionButton(inputId = "genderSA",
                    label = "Select All"),
+      # Race and ethnicity checkboxes
       checkboxGroupInput(inputId = "race",
                          label = "RaceEth: ",
                          choices = sort(unique(final_app_df_10.11.19$RaceEth)),
                          inline = TRUE),
+      # Select All button for race and ethnicity
       actionButton(inputId = "raceSA",
                    label = "Select All"),
+      # Primary language checkboxes
       checkboxGroupInput(inputId = "language",
                          label = "Primary Language: ",
                          choices = sort(unique(final_app_df_10.11.19$Language)),
                          inline = TRUE),
+      # Select All button for primary language
       actionButton(inputId = "languageSA",
                    label = "Select All"),
+      # City checkboxes
       checkboxGroupInput(inputId = "city",
                          label = "City: ",
                          choices = sort(unique(final_app_df_10.11.19$City)),
                          inline = TRUE),
+      # Select All button for cities
       actionButton(inputId = "citySA",
                    label = "Select All"),
+      # Program site checkboxes
       checkboxGroupInput(inputId = "programsite",
                          label = "Program Site: ",
                          choices = sort(unique(final_app_df_10.11.19$Program.Site)),
                          inline = TRUE),
+      # Select All button for program site
       actionButton(inputId = "programsiteSA",
                    label = "Select All"),
+      # Name of school checkboxes
       checkboxGroupInput(inputId = "schoolname",
                          label = "School Name: ",
                          choices = sort(unique(final_app_df_10.11.19$SchoolName)),
                          inline = TRUE),
+      # Select All button for names of schools
       actionButton(inputId = "schoolnameSA",
                    label = "Select All")
     ),
     mainPanel(
-      tabsetPanel(type = "tabs",
+      tabsetPanel(type = "tabs", # 2 tabs: one with the report in table form, the other showing the bar plots for each survey question
                   tabPanel("Report",
                            tableOutput("report"),
                            htmlOutput("message"),
@@ -109,6 +131,7 @@ ui <- fluidPage(
                   )
               )
   ),
+  # HTML/CSS for adjusting the dimensions of the report/plot
   tags$script("$(document).on('shiny:connected', function(event) {
 var myWidth = $(window).width();
               Shiny.onInputChange('shiny_width',myWidth)
@@ -126,6 +149,11 @@ var myWidth = $(window).width();
 # Server
 
 server <- function(input, output, session){
+  
+  # set default to 'Select All' for every variable
+  # if 'Select All' has been clicked an odd number of times, de-select all options
+  # if it's been clicked an even number of times, select all options
+  
   observe({
     if (input$gradeSA%%2 == 0){
       updateCheckboxGroupInput(session,
@@ -246,9 +274,10 @@ server <- function(input, output, session){
     }
   })
   
-  
+  # assemble the dynamic report
   
   reactive_report <- eventReactive(input$update, {
+    # apply all the filters chosen by the user
     final_app_df_10.11.19 %>%
       filter(., Grade %in% input$grade) %>%
       filter(., Gender %in% input$gender) %>%
@@ -257,10 +286,14 @@ server <- function(input, output, session){
       filter(., Language %in% input$language) %>%
       filter(., SchoolName %in% input$schoolname) %>%
       filter(., City %in% input$city) %>%
+    # calculate the percentchange between pre- and post-values
       get_pct_change3(.) %>%
       assign("ink_report", ., envir = .GlobalEnv)
       ink_report
   })
+  
+  # dynamically calculate how many program paticipants meet the criteria specified by the filters
+  
   reactive_count <- eventReactive(input$update, {
     filtered_df <- final_app_df_10.11.19 %>%
       filter(., Grade %in% input$grade) %>%
@@ -270,12 +303,17 @@ server <- function(input, output, session){
       filter(., Language %in% input$language) %>%
       filter(., SchoolName %in% input$schoolname) %>%
       filter(., City %in% input$city)
+    # use HTML to format the number of participants
     print(paste("*There are ", "<font color=\"#FF0000\"><b>", nrow(filtered_df)/2, "</b></font>", " Inkers in this report."))
   })
+  
+  # dynamically produce a barplots showing % change for each survey question, disaggregated by category of question
+  
   reactive_bars <- eventReactive(reactive_report(), {
     bar_report <- ink_report
     bar_report$Pct.Change <- gsub("%", "", ink_report$Pct.Change)
     bar_report <- bar_report %>%
+    # specify when changes are good and when they are bad; color good changes green and bad changes red
       mutate(., Color = factor(case_when(as.numeric(Pct.Change) > 0 & ImproveDirection == "ImproveUp" ~ "green",
                                                         as.numeric(Pct.Change) < 0 & ImproveDirection == "ImproveDown" ~ "green",
                                                         as.numeric(Pct.Change) > 0 & ImproveDirection == "ImproveDown" ~ "red",
@@ -284,6 +322,9 @@ server <- function(input, output, session){
                                                              as.numeric(Pct.Change) < 0 ~ -1.5,
                                                              as.numeric(Pct.Change) == 0 ~ 0.5)) %>%
       left_join(., response_codes[, c("Questions", "Category")])
+    
+    # plot for questions related to attitudes about writing
+    
     attitudes_plot <- ggplot(bar_report %>% filter(., Category == "Attitudes"), aes(x = factor(Questions), y = as.numeric(Pct.Change))) +
       geom_col(aes(x = factor(Questions),
                    y = as.numeric(Pct.Change),
@@ -299,6 +340,9 @@ server <- function(input, output, session){
       theme_bw() +
       theme(plot.title = element_text(size = 16),
             legend.position = "none")
+    
+    # plot about questions related to participant behavior
+    
     behavior_plot <- ggplot(bar_report %>% filter(., Category == "Behavior"), aes(x = factor(Questions), y = as.numeric(Pct.Change))) +
       geom_col(aes(x = factor(Questions),
                    y = as.numeric(Pct.Change),
@@ -314,6 +358,9 @@ server <- function(input, output, session){
       theme_bw() +
       theme(plot.title = element_text(size = 16),
             legend.position = "none")
+    
+    # plot about questions related to social-emotional learning
+    
     socemot_plot <- ggplot(bar_report %>% filter(., Category == "SocEmotional"), aes(x = factor(Questions), y = as.numeric(Pct.Change))) +
       geom_col(aes(x = factor(Questions),
                    y = as.numeric(Pct.Change),
@@ -331,6 +378,9 @@ server <- function(input, output, session){
             legend.position = "none")
     plot_grid(attitudes_plot, behavior_plot, socemot_plot, ncol = 3)
   })
+  
+  # print the report
+  # if any of the variables have no options selected, do not render a report
   output$report <- 
     renderTable({
       if(is.null(input$gender)){
@@ -356,10 +406,14 @@ server <- function(input, output, session){
       }
       reactive_report()
     })
+  
+  # print the count of participants meeting the specified criteria
   output$message <-
     renderText({
       reactive_count()
     })
+  
+  # introductory message to the app
   output$intro <-
     renderText({
       if(input$update == 0){
@@ -369,10 +423,14 @@ server <- function(input, output, session){
         return(NULL)
       }
     })
+  
+  # print the reactive barplot
    output$plot <-
        renderPlot({
          reactive_bars()
          })
+  
+  # option to download the custom barplot
    output$downloadPlot <- downloadHandler(
      filename <- function() {
        paste(input$my_file_name, "png", sep = ".")
